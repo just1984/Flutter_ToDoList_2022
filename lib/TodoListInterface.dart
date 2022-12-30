d909f860d81d879cc2c248151aaa790099d28672
Just161 <bo.niehaus@gmail.com>
2022-12-30 09:25:21 +0100

    developed basic todolist function w/ interface :)

--- lib/TodoListInterface.dart
+++ lib/TodoListInterface.dart
@@ -32,60 +32,93 @@ class _TodoListState extends State<TodoList> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter TodoList ✏️"),
+        backgroundColor: Colors.teal,
+        title: const Text("Meine To-Do-Liste"),
       ),
       body: ListView(
-        children: _getTodoListItem(),
+        children: _getItems(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _displayDialog(context),
-        tooltip: 'Add Item',
-        child: const Icon(Icons.add_box_rounded),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.teal,
+            onPressed: () => _displayDialog(context),
+            tooltip: 'Add Item',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10),
+          const FloatingActionButton(
+            backgroundColor: Colors.teal,
+            onPressed: null,
+            tooltip: 'Reset List',
+            child: Icon(Icons.delete),
+          ),
+        ],
       ),
     );
   }
-}
 
-void _addTodoItem(String title) {
+  void _addTodoItem(String title) {
+    setState(() {
+      todosArray.add(title);
+    });
+    inputFieldController.clear();
 //  importing the human written text in the parameter title to be sent to the ListTile and clearing the input field after successfully adding a new todo
-}
+  }
 
-void _buildTodoItem(String title) {
+  Widget _buildTodoItem(String title) {
 //  receive the text from _addTodoItem and pack it in a ListTile widget
-}
+    return ListTile(
+      title: Text(title),
+    );
+  }
 
-Future<Future> _displayDialog(BuildContext context) async {
+  Future<Future> _displayDialog(BuildContext context) async {
 //  this function will be called when the user clicks on the floating action button to display a dialog box to enter a new todo
-  return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Add a Task to the TodoList"),
-          content: TextField(
-            controller: TextEditingController(),
-            decoration: const InputDecoration(hintText: 'Enter New Todo:'),
-          ),
-          actions: <Widget>[
-            OutlinedButton(
-                onPressed: () {
-                  print("Hello World");
-                },
-                child: const Text("Add")),
-            OutlinedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Cancel")),
-          ],
-        );
-      });
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            title: const Text("Add a Task to the TodoList"),
+            content: TextField(
+              controller: inputFieldController,
+              decoration: const InputDecoration(hintText: 'Enter New Todo:'),
+            ),
+            actions: <Widget>[
+              OutlinedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    _addTodoItem(inputFieldController.text);
+                  },
+                  child: const Text("Add")),
+              OutlinedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Cancel")),
+            ],
+          );
+        });
+  }
+
+  List<Widget> _getItems() {
+    final List<Widget> todoWidget = <Widget>[];
+    for (String title in todosArray) {
+      todoWidget.add(_buildTodoItem(title));
+    }
+    return todoWidget; //Empty Array in the List
+//  this function should return a list of ListTile widgets with the help of a for loop
+  }
 }
 
-List<Widget> _getTodoListItem() {
-  // return empty
-  return []; //Empty Array in the List
-//  this function should return a list of ListTile widgets with the help of a for loop
-}
+// void clearWidget() {
+//   setState(() {
+//     ListTile = null;
+//   });
+// }
